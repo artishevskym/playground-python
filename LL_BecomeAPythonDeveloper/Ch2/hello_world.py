2302af3842725a371e1898e6660384b1ca39a97b
Martin Artishevsky <artishevsky.m@gmail.com>
2021-11-13 19:18:56 +0000

    First app - Hello World

---
+++ LL_BecomeAPythonDeveloper/Ch2/hello_world.py
@@ -0,0 +1,11 @@
+#
+# First app - Hello World
+#
+
+def main():
+    print("hello World Python enthusiasts!")
+    name = input("What is your name? ")
+    print("Nice to meet you,", name)
+
+if __name__ == "__main__":
+    main()
